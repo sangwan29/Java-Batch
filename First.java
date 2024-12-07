5671ebe0eeb5a42394dfd1fff8ff04a5b35c028a
user.name:"sangwan29 <user.email like:"sangwandivya03@gmail.com>
2024-12-07 18:59:01 +0530

    My java first program

---
+++ First.java
@@ -0,0 +1,77 @@
+//java program of hello world 
+/*public class First{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
+*/
+
+//five friends 
+/*
+public class First{
+    public static void main(String [] args){
+        System.out.print("Divya");
+        System.out.print("Meghna");
+        System.out.print("Kashii");
+        System.out.print("Sushma");
+        System.out.print("Rekha");
+    }
+}
+*/
+//five fav food name :new line
+/*
+public class First{
+    public static void main(String[] args){
+        System.out.println("Pizza");
+        System.out.println("Burger");
+        System.out.println("Paratha");
+        System.out.println("Aloo Tikki");
+        System.out.println("Chole Bhature");
+    }
+}
+*/
+
+//five city name :new line 
+/*
+public class First{
+    public static void main(String[] args){
+        System.out.println("Jaipur");
+        System.out.println("Chandigarh");
+        System.out.println("Bhiwani");
+        System.out.println("Sikar");
+        System.out.println("Udaipur");
+
+    }
+}
+*/
+
+what is java ?
+Java is a programming language and a platform. Java is a high level, robust, object-oriented and secure programming language.
+
+what is the features of java ?
+Simple:
+    Java is very easy to learn, and its syntax is simple, clean and easy to understand.
+Object-Oriented:
+    Java is an object-oriented programming language. Everything in Java is an object. Object-oriented means we organize our software as a combination of different types of objects
+Portable:
+    Java is portable because it facilitates you to carry the Java bytecode to any platform. It does not require any implementation  
+Platform independent:
+      Java is platform independent because Java is a write once, run anywhere language.
+Secured:
+    Java is best known for its security. With Java, we can develop virus-free systems.
+Robust:
+    The English mining of Robust is strong. Java is robust because:It uses strong memory management.There is a lack of pointers that avoids security problems.
+Architecture neutral:
+    Java is architecture neutral because there are no implementation dependent features, for example, the size of primitive types is fixed.
+Interpreted:
+    Java is faster than other traditional interpreted programming languages because Java bytecode is "close" to native code. 
+High Performance:
+    Java is faster than other traditional interpreted programming languages because Java bytecode is "close" to native code. 
+Multithreaded:
+    We can write Java programs that deal with many tasks at once by defining multiple threads. The main advantage of multi-threading is that it does not occupy memory for each thread. It shares a common memory area. Threads are important for multi-media, Web applications, etc.
+Distributed:
+    Java is distributed because it facilitates users to create distributed applications in Java.This feature of Java makes us able to access files by calling the methods from any machine on the internet.
+Dynamic:
+   Java is a dynamic language. It supports the dynamic loading of classes. It means classes are loaded on demand. 
+
+   
